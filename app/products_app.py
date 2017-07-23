10557f86633d1a47e18c33c7db6c9d2af7fab1ff
ECDODD <ecd339@nyu.edu>
2017-07-22 22:00:45 -0400

    Read existing inventory from csv and overwrite the same file.

--- app/products_app.py
+++ app/products_app.py
@@ -26,27 +26,36 @@ menu = """
 
 """.format(len(products))
 
-crud_operation = input(menu)
-crud_operation = crud_operation.title()
+#other_path = "data/other_products.csv"
 
-def list_products():
-    print("LISTING PRODUCTS")
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)
 
-def show_product():
-    print("SHOWING PRODUCT")
 
-def create_product():
-    print("CREATING PRODUCT")
-
-def update_product():
-    print("UPDATING PRODUCT")
-
-def destroy_product():
-    print("DESTROYING PRODUCT")
-
-if crud_operation == "List": list_products()
-elif crud_operation == "Show": show_product()
-elif crud_operation == "Create": create_product()
-elif crud_operation == "Update": update_product()
-elif crud_operation == "Destroy": destroy_product()
-else: print("OOPS. PLEASE CHOOSE OF THE RECOGNIZED OPERATIONS.")
+# crud_operation = input(menu)
+# crud_operation = crud_operation.title()
+#
+# def list_products():
+#     print("LISTING PRODUCTS")
+#
+# def show_product():
+#     print("SHOWING PRODUCT")
+#
+# def create_product():
+#     print("CREATING PRODUCT")
+#
+# def update_product():
+#     print("UPDATING PRODUCT")
+#
+# def destroy_product():
+#     print("DESTROYING PRODUCT")
+#
+# if crud_operation == "List": list_products()
+# elif crud_operation == "Show": show_product()
+# elif crud_operation == "Create": create_product()
+# elif crud_operation == "Update": update_product()
+# elif crud_operation == "Destroy": destroy_product()
+# else: print("OOPS. PLEASE CHOOSE OF THE RECOGNIZED OPERATIONS.")
