aea58931e841ee7ea5c820991d442d910c9aa415
ECDODD <ecd339@nyu.edu>
2017-07-22 20:05:57 -0400

    Demonstrate ability to read csv file.

--- app/products_app.py
+++ app/products_app.py
@@ -3,51 +3,50 @@ import csv
 
 # READ PRODUCTS CSV
 
-products = []
-
 csv_file_path = "data/products.csv"
 
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
-        products.append(row)
+        print(row["id"], row["name"])
 
-menu = """
-    Hi.
 
-    Welcome to the products app.
 
-    There are {0} products.
-
-    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
-
-    Please choose an operation:
-
-""".format(len(products))
-
-print(menu)
-
-#chosen_operation = input(menu)
-#chosen_operation = chosen_operation.title()
+# #products = []
 #
-#def list_products():
-#    print("LISTING PRODUCTS")
+# menu = """
+#     Hi.
 #
-#def show_product():
-#    print("SHOWING A PRODUCT")
+#     Welcome to the products app.
 #
-#def create_product():
-#    print("CREATING A PRODUCT")
+#     There are {0} products.
 #
-#def update_product():
-#    print("UPDATING A PRODUCT")
+#     Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
 #
-#def destroy_product():
-#    print("DESTROYING A PRODUCT")
+#     Please choose an operation:
 #
-#if chosen_operation == "List": list_products()
-#elif chosen_operation == "Show": show_product()
-#elif chosen_operation == "Create": create_product()
-#elif chosen_operation == "Update": update_product()
-#elif chosen_operation == "Destroy": destroy_product()
-#else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+# """
+# crud_operation = input(menu)
+# crud_operation = crud_operation.title()
+#
+# def list_products():
+#     print("LISTING PRODUCTS")
+#
+# def show_product():
+#     print("SHOWING PRODUCT")
+#
+# def create_product():
+#     print("CREATING PRODUCT")
+#
+# def update_product():
+#     print("UPDATING PRODUCT")
+#
+# def destroy_product():
+#     print("DESTROYING PRODUCT")
+#
+# if crud_operation == "List": list_products()
+# elif crud_operation == "Show": show_product()
+# elif crud_operation == "Create": create_product()
+# elif crud_operation == "Update": update_product()
+# elif crud_operation == "Destroy": destroy_product()
+# else: print("OOPS. PLEASE CHOOSE OF THE RECOGNIZED OPERATIONS.")
